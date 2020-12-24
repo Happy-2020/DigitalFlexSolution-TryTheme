46b27d8f9c45b3f07b19688f81b95401b0ede674
Happy-2020 <babystar6666@outlook.com>
2020-12-24 15:17:07 +0900

    Update baby menu face

--- src/components/ui/navbar/babymenu/babymenu.style.js
+++ src/components/ui/navbar/babymenu/babymenu.style.js
@@ -7,9 +7,9 @@ export const BabymenuWrap = styled.ul `
     box-shadow: 0 2px 29px rgba(0,0,0,0.05);
     border-bottom: 3px solid #fd5b01;
     background-color: #000000;
-    transform: translateX(150px);
+    transform: translateX(-150px);
     transition: all 0.3s cubic-bezier(0.645, 0.045, 0.355, 1);
-    transition-delay: 0.2s;
+    transition-delay: 0.1s;
     transition-duration: 0.4s;
     visibility: hidden;
     opacity: 0;
