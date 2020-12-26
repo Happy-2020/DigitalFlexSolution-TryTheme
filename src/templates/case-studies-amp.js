57e0001706add4adf0cccdbfc3755d050fa2a101
Happy-2020 <babystar6666@outlook.com>
2020-12-26 13:30:55 +0900

    Update node.js --4

--- src/templates/case-studies-amp.js
+++ src/templates/case-studies-amp.js
@@ -8,21 +8,20 @@ import CaseStudyArea from '../containers/global/case-study-area'
 import CTAArea from '../containers/global/cta-area/section-one'
 
 const CaseStudiesPage = ({ pageContext, location }) => (
-  <Layout location={location}>
-    <SEO title="Case Studies"/>
-    <Header/>
-    <PageHeader 
-        pageContext={pageContext} 
-        location={location}
-        title="Case Studies"
-    />
-    <main className="site-wrapper-reveal">
-        <CaseStudyArea/>
-        <CTAArea/>
-    </main>
-    <Footer/>
-  </Layout>
+    <Layout location={location}>
+        <SEO title="Case Studies" />
+        <Header />
+        <PageHeader
+            pageContext={pageContext}
+            location={location}
+            title="Case Studies"
+        />
+        <main className="site-wrapper-reveal">
+            <CaseStudyArea />
+            <CTAArea />
+        </main>
+        <Footer />
+    </Layout>
 )
- 
+
 export default CaseStudiesPage
- 
